32166bff965b8663628d60bd85e47b2ac8bad7dd
Rodrigo Lima <rodrigo-l-c@hotmail.com>
2023-05-29 22:03:53 -0300

    Inicialização do repositório

---
+++ func.py
@@ -0,0 +1,71 @@
+opcs_validas = """
+0|1|2
+3|4|5
+6|7|8
+"""
+
+
+def printar():
+    print("oi")
+
+
+def valida_escolha(prim_escolha, seg_escolha):
+    if prim_escolha > 8:
+        print("A primeira opção está fora dos valores permitidos")
+        print("Selecione Novamente !!")
+        return False
+    elif seg_escolha > 8:
+        print("A segunda opção está fora dos valores permitidos")
+        print("Selecione Novamente !!")
+        return False
+    elif seg_escolha == prim_escolha:
+        print("Os quadrados não podem ser iguais")
+        print("Selecione Novamente !!")
+        return False
+    else:
+        return True
+
+
+casas = [["  "], ["  "], ["  "],
+         ["  "], ["  "], ["  "],
+         ["  "], ["  "], ["  "],]
+
+
+def marca_quadrados(simbolo, num_jogada, prim_escolha, seg_escolha):
+
+    if (casas[seg_escolha][0] == "  "):
+        casas[seg_escolha][0] = (f'{simbolo}{num_jogada}')
+    else:
+        casas[seg_escolha].append(f'{simbolo}{num_jogada}')
+
+    if (casas[prim_escolha][0] == "  "):
+        casas[prim_escolha][0] = (f'{simbolo}{num_jogada}')
+    else:
+        casas[prim_escolha].append(f'{simbolo}{num_jogada}')
+
+    espacamento = 1
+
+    for b in range(8):
+        if len(casas[b]) > espacamento:
+            print(casas[b], b)
+            espacamento = len(casas[b])
+
+    print("espaçamento: ", espacamento)
+
+    for i in range(9):
+
+        if (i % 3 == 0):
+            print()
+        else:
+            print(" | ", end="")
+
+        if (len(casas[i]) != espacamento):
+            qntd_de_espaco = espacamento - len(casas[i])
+            for opcs in casas[i]:
+                print(opcs, end=" ")
+            espaco = "   "
+            print(f"{espaco*qntd_de_espaco}", end="")
+
+        else:
+            for a in casas[i]:
+                print(a, end=" ")
---
+++ jogo.py
@@ -0,0 +1,32 @@
+# Importando funções
+from func import *
+
+nomes = ["O", "X"]
+simb = ["O", "X"]
+# nomes[1] = input("Digite o nome do jogador X: ")
+# nomes[0] = input("Digite o nome do jogador O: ")
+
+nomes[1] = "Rodrigo"
+nomes[0] = "Ana"
+
+
+# Começo do jogo
+vez = True  # VEZ == TRUE -> X | VEZ == FALSE -> O
+num_jogada = 0  # Numero da jogada
+while True:
+    num_jogada += 1
+    if (vez):
+
+        print(
+            f"{num_jogada}° - Vez do jogador {simb[vez]}, selecione uma casa {nomes[vez]}")
+        print(opcs_validas)
+
+        while True:
+            print("=====================================")
+            prim_jogada = int(input("Digite sua primeira casa:"))
+            seg_jogada = int(input("Digite sua segunda casa:"))
+            if valida_escolha(prim_jogada, seg_jogada):
+                vez != vez  # muda a vez
+                marca_quadrados(simb[vez], num_jogada, prim_jogada, seg_jogada)
+                # verifica_jogada(vez, prim_jogada, seg_jogada)
+                break
